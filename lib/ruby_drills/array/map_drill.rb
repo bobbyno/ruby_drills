9bd37803816e2084437ee777567d2adddfed8ed8
Bobby Norton <bobby@testedminds.com>
2014-01-21 20:03:15 -0600

    Add a map drill

---
+++ lib/ruby_drills/array/map_drill.rb
@@ -0,0 +1,28 @@
+class MapDrill < Drill
+
+  def setup
+    @numbers = (0..10).to_a
+    @hints = ["You want to divide each member of this collection by two..."
+              "http://ruby-doc.org/core-1.9.3/Enumerable.html#method-i-map"]
+  end
+
+  def show
+    puts %{
+@numbers = #{@numbers.inspect}
+
+Use the method that returns a new array with the results of
+running block once for every element in enum to produce
+#{expected}
+
+}
+  end
+
+  def reference
+    "@numbers.map {|x| x / 2}"
+  end
+
+  def valid?(input)
+    ['map', 'collect'].any? {|x| input.include? x}
+  end
+
+end
