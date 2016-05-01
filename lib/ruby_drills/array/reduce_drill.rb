8340b1129d3cef10911a1cc89a76f823bad46ce8
Bobby Norton <bobby@testedminds.com>
2016-05-01 16:55:40 -0500

    Provide cleaner example for mean

--- lib/ruby_drills/array/reduce_drill.rb
+++ lib/ruby_drills/array/reduce_drill.rb
@@ -1,7 +1,7 @@
 class ReduceDrill < Drill
 
   def setup
-    @values = [0, 5, 19, 25, 23, 40, 55, 36, 32, 52]
+    @values = [3, 5, 19, 25, 23, 40, 55, 36, 32, 52]
     @hints = ["How could you reduce this problem to something simpler?",
               "TODO: Inject a hint here...",
               "This method is called foldLeft in some languages.",
