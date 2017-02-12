ba07069f02cf01f51caff07fe44d987c6770e018
Sarah Aslanifar <Sarah.Aslanifar@morningstar.com>
2017-02-11 23:09:02 -0600

    add delete and uniq array drills

---
+++ lib/ruby_drills/array/delete_drill.rb
@@ -0,0 +1,24 @@
+class DeleteDrill < Drill
+
+  def setup
+    @values = [ "a", "b", "b", "b", "c" ]
+    @hints = ["It's the method you think it is!",
+                  "https://ruby-doc.org/core-2.3.0/Array.html#method-i-delete"]
+  end
+
+  def show
+    puts %{
+@values = #{@values.inspect}
+Use a method to delete "b" from the array of values.
+}
+  end
+
+  def reference
+    "@values.delete(\"b\")"
+  end
+
+  def valid?(input)
+    input.include?("delete(\"b\")")
+  end
+
+end
